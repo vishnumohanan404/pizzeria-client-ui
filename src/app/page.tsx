fb0c3bcf4dbb5e9606a2719528a83442339d604f
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2025-06-01 07:17:52 +0530

    add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return <main className="">
     <h1>Welcome</h1>
+    <Button variant="outline">Button</Button>
   </main>;
 }
