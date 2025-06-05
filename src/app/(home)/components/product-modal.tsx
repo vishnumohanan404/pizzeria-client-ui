e35acd8fb91df942dba6ae52c2126e33a041222b
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2025-06-05 21:25:38 +0530

    render availableOptions dynamically

---
+++ src/app/(home)/components/product-modal.tsx
@@ -0,0 +1,87 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import ToppingList from "./topping-list";
+import { Button } from "@/components/ui/button";
+import { ShoppingCart } from "lucide-react";
+import Image from "next/image";
+import { Product } from "@/lib/types";
+
+const ProductModal = ({ product }: { product: Product }) => {
+  console.log("product :>> ", product);
+  return (
+    <Dialog>
+      <DialogTrigger className="bg-orange-200 hover:bg-orange-300 text-orange-500 px-6 py-2 rounded-full shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150">
+        Choose
+      </DialogTrigger>
+      <DialogContent className="min-w-3xl p-0 ">
+        <VisuallyHidden>
+          <DialogTitle></DialogTitle>
+        </VisuallyHidden>
+        <div className="flex">
+          <div className="w-1/3 bg-white rounded p-8 flex items-center justify-center">
+            <Image
+              src={product.image}
+              width={450}
+              height={450}
+              alt={product.name}
+            />
+          </div>
+          <div className="w-2/3 p-8">
+            <h3 className="text-xl font-bold">{product.name}</h3>
+            <p className="mt-1">{product.description}</p>
+            {Object.entries(product.category.priceConfiguration).map(
+              ([key, value]) => {
+                return (
+                  <div key={key}>
+                    <h4 className="mt-6">Choose the {key}</h4>
+                    <RadioGroup
+                      defaultValue={value.availableOptions[0]}
+                      className="grid grid-cols-3 gap-4 mt-2"
+                    >
+                      {value.availableOptions?.map((option) => {
+                        return (
+                          <div key={option}>
+                            <RadioGroupItem
+                              value={option}
+                              id={option}
+                              className="peer sr-only"
+                              aria-label={option}
+                            />
+                            <Label
+                              htmlFor={option}
+                              className="flex flex-col items-center justify-between rounded-md border-2 bg-white p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                            >
+                              {option}
+                            </Label>
+                          </div>
+                        );
+                      })}
+                    </RadioGroup>
+                  </div>
+                );
+              }
+            )}
+
+            <ToppingList />
+            <div className="flex items-center justify-between mt-12">
+              <span className="font-bold">&#8377; 400</span>
+              <Button>
+                <ShoppingCart size={20} />
+                <span className="ml-1">Add to cart</span>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ProductModal;
