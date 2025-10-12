fc191e2c4ff6b9e735f9ae2081685dd0612def69
Vishnu Mohan <vishnumohan1996nov@email.com>
2025-10-12 15:15:15 +0530

    fix cart items count

---
+++ src/components/custom/cart-counter.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useAppDispatch, useAppSelector } from "@/lib/store/hooks/hooks";
+import { ShoppingBasket } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+const CartCounter = () => {
+  const dispatch = useAppDispatch();
+  const cartItems = useAppSelector((state) => state.cart.cartItems);
+  return (
+    <>
+      <div className="relative">
+        <Link href={"/cart"}>
+          <ShoppingBasket className="hover:text-primary" />
+        </Link>
+        <span className="absolute -top-4 -right-5 h-6 w-6 flex items-center justify-center rounded-full bg-primary font-bold text-white">
+          {cartItems.length}
+        </span>
+      </div>
+    </>
+  );
+};
+
+export default CartCounter;
--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -10,6 +10,7 @@ import Link from "next/link";
 import { Phone, ShoppingBasket } from "lucide-react";
 import { Button } from "../ui/button";
 import { Tenant } from "@/lib/types";
+import CartCounter from "./cart-counter";
 
 const Header = async () => {
   const tenantsResponse = await fetch(
@@ -75,6 +76,7 @@ const Header = async () => {
               </Link>
             </li>
           </ul>
+          <CartCounter />
           <div className="flex items-center ml-12">
             <Phone />
             <span>+91 9790 289 300</span>
